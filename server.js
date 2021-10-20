16b1c4b5d441fc63a5dd6e196761998c9bd6729b
Matt Weichel <maweiche@gmail.com>
2021-10-19 18:05:47 -0700

    added createNote function

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ const fs = require('fs');
 const path = require('path');
 const PORT = 3001;
 const app = express();
+
+
+
+//request data
+const notes  = require('./db/db.json');
+
+//function for taking data and adding it to db.json
+function createNote (body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+}
