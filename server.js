fbdc039f1ee765c7bd36a2e310f30c1e5defddbd
Matt Weichel <maweiche@gmail.com>
2021-10-21 08:27:28 -0700

    changed index url from '/' to '*'

--- server.js
+++ server.js
@@ -73,14 +73,14 @@ app.delete('/api/notes/:id', (req, res) =>{
     })
 });
 
-//route to index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 //route to notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//route to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 //add listen method
